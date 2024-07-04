aa35da056e3ccaa224938b1601289419ae28d790
Juliandev02 <julian@strawberryfoundations.xyz>
2024-07-05 00:01:16 +0200

    feat: some improvements to binpkg

--- src/binpkg/mod.rs
+++ src/binpkg/mod.rs
@@ -1,4 +1,9 @@
-use serde_json::Value;
+use std::fs::File;
+use std::io::Write;
+use flate2::Compression;
+use flate2::write::GzEncoder;
+use tar::Builder;
+
 use crate::binpkg::metadata::Metadata;
 
 pub mod create;
@@ -6,12 +11,25 @@ pub mod read;
 pub mod metadata;
 
 pub struct BinPkg {
-    pub header: Value,
     pub metadata: Metadata,
 }
 
 impl BinPkg {
-    pub fn create(metadata: Metadata) -> Self {
+    pub fn create(metadata: Metadata, source_directory: impl ToString, output_file: impl ToString) -> Self {
+        let metadata_json = serde_json::to_string(&metadata).unwrap();
+        let metadata_length = metadata_json.len();
+        let header = format!("LENGTH={}\n{}", metadata_length, metadata_json);
 
+        let mut output = File::create(output_file.to_string()).unwrap();
+        output.write_all(header.as_bytes()).unwrap();
+        output.write_all(b"\n").unwrap();
+
+        let tar_gz = GzEncoder::new(output, Compression::default());
+        let mut tar = Builder::new(tar_gz);
+        tar.append_dir_all(".", source_directory.to_string()).unwrap();
+            
+        Self {
+            metadata
+        }
     }
 }
