2cc3cb29b128bb5ebc2c96c89bf5f7acae6099c0
Juliandev02 <julian@strawberryfoundations.xyz>
2024-07-04 23:51:15 +0200

    feat: Added metadata

--- src/binpkg/mod.rs
+++ src/binpkg/mod.rs
@@ -1,2 +1,17 @@
+use serde_json::Value;
+use crate::binpkg::metadata::Metadata;
+
 pub mod create;
 pub mod read;
+mod metadata;
+
+pub struct BinPkg {
+    pub header: Value,
+    pub metadata: Metadata,
+}
+
+impl BinPkg {
+    pub fn create(metadata: Metadata) -> Self {
+
+    }
+}
