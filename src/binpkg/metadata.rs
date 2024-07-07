3ee527d4aab5d6875a1dae817dad355d59391b92
Juliandev02 <julian@strawberryfoundations.xyz>
2024-07-07 20:40:00 +0200

    feat: added additional fields for binpkg

--- src/binpkg/metadata.rs
+++ src/binpkg/metadata.rs
@@ -3,16 +3,29 @@ use serde::{Deserialize, Serialize};
 #[derive(Serialize, Deserialize)]
 pub struct Metadata {
     pub name: String,
+    pub id: String,
     pub version: String,
     pub description: String,
+    pub architecture: String,
+    pub author: String,
 }
 
 impl Metadata {
-    pub fn new(name: impl ToString, version: impl ToString, description: impl ToString) -> Self {
+    pub fn new(
+        name: impl ToString,
+        id: impl ToString,
+        version: impl ToString,
+        description: impl ToString,
+        architecture: impl ToString,
+        author: impl ToString
+    ) -> Self {
         Self {
             name: name.to_string(),
+            id: id.to_string(),
             version: version.to_string(),
-            description: description.to_string()
+            description: description.to_string(),
+            architecture: architecture.to_string(),
+            author: author.to_string(),
         }
     }
 
@@ -21,6 +34,6 @@ impl Metadata {
     }
     
     pub fn from_from(json: &str) -> Self {
-        serde_json::from_str(&json).unwrap()
+        serde_json::from_str(json).unwrap()
     }
 }
