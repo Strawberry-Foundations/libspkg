49d0011f281b603041cf70c9278821378808a1c2
Juliandev02 <julian@strawberryfoundations.xyz>
2024-07-07 17:10:33 +0200

    feat: improved error handling

--- src/binpkg/mod.rs
+++ src/binpkg/mod.rs
@@ -12,24 +12,28 @@ pub mod metadata;
 
 pub struct BinPkg {
     pub metadata: Metadata,
+    pub source: Option<String>,
+    pub output: Option<String>,
 }
 
 impl BinPkg {
-    pub fn create(metadata: Metadata, source_directory: impl ToString, output_file: impl ToString) -> Self {
-        let metadata_json = serde_json::to_string(&metadata).unwrap();
+    pub fn create(metadata: Metadata, source_directory: impl ToString, output_file: impl ToString) -> eyre::Result<Self> {
+        let metadata_json = serde_json::to_string(&metadata)?;
         let metadata_length = metadata_json.len();
         let header = format!("LENGTH={}\n{}", metadata_length, metadata_json);
 
-        let mut output = File::create(output_file.to_string()).unwrap();
-        output.write_all(header.as_bytes()).unwrap();
-        output.write_all(b"\n").unwrap();
+        let mut output = File::create(output_file.to_string())?;
+        output.write_all(header.as_bytes())?;
+        output.write_all(b"\n")?;
 
         let tar_gz = GzEncoder::new(output, Compression::default());
         let mut tar = Builder::new(tar_gz);
-        tar.append_dir_all(".", source_directory.to_string()).unwrap();
-            
-        Self {
-            metadata
-        }
+        tar.append_dir_all(".", source_directory.to_string())?;
+                 
+        Ok(Self {
+            metadata,
+            source: Some(source_directory.to_string()),
+            output: Some(output_file.to_string())
+        })
     }
 }
